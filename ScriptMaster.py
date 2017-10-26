9e1d3d05593a8909defd42832cc7d248d3d829e6
dzhukov40 <dzhukov40.v@mail.ru>
2017-10-27 01:34:41 +0300

    clean main script

--- ScriptMaster.py
+++ ScriptMaster.py
@@ -35,37 +35,6 @@ def tort(msg):
 
         bot.sendMessage(chatId, chatText)
 
-    # messageChatId = BotUtility.getMessageChatId(msg)
-
-    # bot.sendMessage(messageChatId, 'приветики')
-    # bot.sendPhoto(messageChatId, open('test_data/Photo_1.jpg', 'rb'), caption='тетя')
-    # bot.sendPhoto(messageChatId, 'http://dummyimage.com/600x400/000/fff.png&text=telegram')
-    #
-    # bot.sendAudio(messageChatId, open('test_data/Song_2.mp3', 'rb'))
-    # bot.sendAudio(messageChatId, 'https://python-telegram-bot.org/static/testfiles/telegram.mp3')
-    #
-    # bot.sendDocument(messageChatId, open('test_data/File_1.txt', 'rb'))
-    # bot.sendDocument(messageChatId, 'http://lelang.ru/wp-content/pdf_noindex_medium/Eveline.pdf')
-    #
-    # bot.sendVideo(messageChatId, open('test_data/Video_1.mp4', 'rb'))
-    # bot.sendAudio(messageChatId, 'https://python-telegram-bot.org/static/testfiles/telegram.mp4')
-    #
-    # bot.sendVoice(messageChatId, open('test_data/Voice_1.ogg', 'rb'))
-    # bot.sendVoice(messageChatId, 'https://python-telegram-bot.org/static/testfiles/telegram.ogg')
-    #
-    # bot.sendVideoNote(messageChatId, open('test_data/Video_1.mp4', 'rb'))
-    # bot.sendVideoNote(messageChatId, 'https://python-telegram-bot.org/static/testfiles/telegram.mp4')
-
-    # bot.sendLocation(messageChatId, 55.807255, 37.621902)
-
-    # 'https://python-telegram-bot.org/static/testfiles/telegram.jpg'
-    # 'https://python-telegram-bot.org/static/testfiles/telegram.gif'
-    # 'https://python-telegram-bot.org/static/testfiles/telegram.mp4'
-    # 'https://python-telegram-bot.org/static/testfiles/telegram.ogg'
-    # 'https://python-telegram-bot.org/static/testfiles/telegram.webp'
-
-    # bot.stopListen()
-
 
 def main():
 
